71dad1df27b3a769bbfc6adb98098653cd823880
HaneeshSai <hanishsai79@gmail.com>
2023-12-24 11:15:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send({
+    subject: "hi",
+    message: "fuck u",
+  });
+});
+
+app.post("/download", (req, res) => {
+  const link = req.body.videoUrl;
+
+  const video = ytdl(link, { filter: "audioonly" });
+  video.pipe(fs.createWriteStream("./audio/audio.mp3"));
+  console.log("downloaded");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
